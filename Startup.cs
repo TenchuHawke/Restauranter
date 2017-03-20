258f58df0625c50f76ed33c5afaea2abb4f2b83d
TenchuHawke <hawke@unrestrainedgames.com>
2017-03-20 13:04:56 -0700

    updated custom attributes

--- Startup.cs
+++ Startup.cs
@@ -1,43 +1,38 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using RESTauranter.Models;
 using MySQL.Data.EntityFrameworkCore.Extensions;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Hosting;
+using RESTauranter.Models;
 
-namespace RESTauranter
-{
-    public class Startup
-    {
+namespace RESTauranter {
+    public class Startup {
         public IConfiguration Configuration { get; private set; }
 
-        public Startup(IHostingEnvironment env)
-{
-    var builder = new ConfigurationBuilder()
-    .SetBasePath(env.ContentRootPath)
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables();
-    Configuration = builder.Build();
-}
+        public Startup (IHostingEnvironment env) {
+            var builder = new ConfigurationBuilder ()
+                .SetBasePath (env.ContentRootPath)
+                .AddJsonFile ("appsettings.json", optional : true, reloadOnChange : true)
+                .AddEnvironmentVariables ();
+            Configuration = builder.Build ();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             // Add framework services.
-            services.AddMvc();
-            services.AddSession();
-            services.AddDbContext<RESTauranterContext>(options => options.UseMySQL(Configuration["DBInfo:ConnectionString"]));
+            services.AddMvc ();
+            services.AddSession ();
+            services.AddDbContext<RESTauranterContext> (options => options.UseMySQL (Configuration["DBInfo:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-            app.UseDeveloperExceptionPage();
-            app.UseStaticFiles();
-            app.UseSession();
-            app.UseMvc();
+        public void Configure (IApplicationBuilder app, ILoggerFactory loggerFactory) {
+            loggerFactory.AddConsole ();
+            app.UseDeveloperExceptionPage ();
+            app.UseStaticFiles ();
+            app.UseSession ();
+            app.UseMvc ();
         }
     }
 }
--- Utilities/CustomAttributes.cs
+++ Utilities/CustomAttributes.cs
@@ -1,30 +1,19 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 namespace RESTauranter {
-    public class InThePast : RangeAttribute {
-        public InThePast (): base (typeof (DateTime),
-            DateTime.Now.AddYears (-100).ToString (),
-            DateTime.Now.ToString ()) {
-        }
-        public override bool IsValid (object value) {
-            return (((DateTime) value > (DateTime) Minimum) && ((DateTime) value < (DateTime) Maximum));
-        }
-    }
-}
-
-public class PastDated  : ValidationAttribute {
+    public class InThePast : ValidationAttribute {
         private DateTime CurrentDate;
 
-        public PastDated() {
+        public InThePast () {
             CurrentDate = DateTime.Now;
         }
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            DateTime setVal = (DateTime)value;
-            if(setVal < CurrentDate) {
+        protected override ValidationResult IsValid (object value, ValidationContext validationContext) {
+            DateTime setVal = (DateTime) value;
+            if (setVal < CurrentDate) {
                 return ValidationResult.Success;
             }
-            return new ValidationResult("Date of visit must be dated for the past!");
+            return new ValidationResult ("Date of visit must be in the past!");
         }
     }
+}
